b23444ea0227065bfa3e46cf00531af7ebbd4640
pawellll <pmpeksa@gmail.com>
2015-12-14 18:42:54 +0100

    add test for solver, move to proper folders

--- src/Solver.py
+++ src/Solver.py
@@ -1,8 +1,7 @@
-import sys
 import math
 
 
-class Solver():
+class Solver(object):
     def __init__(self, grid=None):
         self.sudoku_grid = [[0 for x in range(9)] for x in range(9)]
         self.size = len(grid) if grid else 9
@@ -10,21 +9,21 @@ class Solver():
         if grid:
             self.sudoku_grid = grid
 
-    def validate_row(self, r, c):
+    def _validate_row(self, r, c):
         value = self.sudoku_grid[r][c]
         for _c in range(self.size):
             if _c != c and self.sudoku_grid[r][_c] == value:
                 return False
         return True
 
-    def validate_column(self, r, c):
+    def _validate_column(self, r, c):
         value = self.sudoku_grid[r][c]
         for _r in range(self.size):
             if _r != r and self.sudoku_grid[_r][c] == value:
                 return False
         return True
 
-    def validate_box(self, r, c):
+    def _validate_box(self, r, c):
         value = self.sudoku_grid[r][c]
         box_r = int(math.floor(r / self.box_size))
         box_c = int(math.floor(c / self.box_size))
@@ -34,7 +33,7 @@ class Solver():
                     return False
         return True
 
-    def backtrack(self, r, c):
+    def _backtrack(self, r, c):
         c += 1
         if c > self.size - 1:
             c = 0
@@ -43,14 +42,14 @@ class Solver():
                 return True
 
         if self.sudoku_grid[r][c] != 0:
-            if not (self.validate_row(r, c) and self.validate_column(r, c) and self.validate_box(r, c)):
+            if not (self._validate_row(r, c) and self._validate_column(r, c) and self._validate_box(r, c)):
                 return False
-            return self.backtrack(r, c)
+            return self._backtrack(r, c)
         else:
             for x in range(1, self.size + 1):
                 self.sudoku_grid[r][c] = x
-                if self.validate_row(r, c) and self.validate_column(r, c) and self.validate_box(r, c):
-                    if (self.backtrack(r, c)):
+                if self._validate_row(r, c) and self._validate_column(r, c) and self._validate_box(r, c):
+                    if (self._backtrack(r, c)):
                         return True
             self.sudoku_grid[r][c] = 0
             return False
@@ -58,37 +57,15 @@ class Solver():
     def solve(self):
         for r in range(self.size):
             for c in range(self.size):
-                if self.sudoku_grid[r][c] != 0 and not self.validate_row(r, c) and self.validate_column(r,
-                                                                                                        c) and self.validate_box(
+                if self.sudoku_grid[r][c] != 0 and not self._validate_row(r, c) and self._validate_column(r,
+                                                                                                        c) and self._validate_box(
                         r, c):
                     return False
-        return self.backtrack(0, -1)
+        return self._backtrack(0, -1)
 
     def print_grid(self):
         for row in self.sudoku_grid:
             print(row)
 
 
-if __name__ == "__main__":
-    grid = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
-            [6, 0, 0, 1, 9, 5, 0, 0, 0],
-            [0, 9, 8, 0, 0, 0, 0, 6, 0],
-            [8, 0, 0, 0, 6, 0, 0, 0, 3],
-            [4, 0, 0, 8, 0, 3, 0, 0, 1],
-            [7, 0, 0, 0, 2, 0, 0, 0, 6],
-            [0, 6, 0, 0, 0, 0, 2, 8, 0],
-            [0, 0, 0, 4, 1, 9, 0, 0, 5],
-            [0, 0, 0, 0, 8, 0, 0, 7, 9]]
 
-    grid = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-    ]
-
-    sudoku = Solver(grid);
-    if sudoku.solve():
-        sudoku.print_grid()
-    else:
-        print("Brak rozwiazania")
